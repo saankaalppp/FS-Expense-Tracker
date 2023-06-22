dab3d3dfcc5e32ac8d833f04ee7f3d0bbc6bc8e0
saankaalppp <sankalp18v@gmail.com>
2023-06-22 22:14:29 +0530

    Making Expense App Production ready

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,14 +1,15 @@
 const Expense = require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database');
 // getExpenses
 
 module.exports.getExpenses = async (req, res, next) => {
-    try{
+    try {
         const expenses = await req.user.getExpenses();
         return res.status(200).json({ expenses: expenses, success: true });
-    } catch(err) {
+    } catch (err) {
         // console.log(err);
-        return res.status(500).json({ message: "Some error occurred" , success: false });
+        return res.status(500).json({ message: "Some error occurred", success: false });
     }
 }
 
@@ -16,18 +17,23 @@ module.exports.getExpenses = async (req, res, next) => {
 
 module.exports.postAddExpense = async (req, res, next) => {
     try {
-        await req.user.createExpense({
+        const user = req.user;
+
+        await user.createExpense({
             amount: req.body.amount,
             description: req.body.description,
             category: req.body.category
-        });
+        }, { transaction: t });
         const user = req.user;
 
         user.totalExpense = Number(user.totalExpense) + Number(req.body.amount);
 
-        await user.save();
+        await user.save({ transaction: t });
+
+        await t.commit();
         return res.status(200).json({ success: true });
-    } catch(err) {
+    } catch (err) {
+        await t.rollback();
         console.log(err);
         return res.status(500).json({ message: "Some error occurred", success: false });
     }
@@ -36,13 +42,27 @@ module.exports.postAddExpense = async (req, res, next) => {
 // postDeleteExpense
 
 module.exports.postDeleteExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();
     try {
-        const expenses = await req.user.getExpenses({ where: { id: req.body.expenseId }});
+        const expenseId = req.body.expenseId;
+        if (expenseId == undefined || expenseId.length === 0)
+            return res.status(400).json({ message: 'Bad request', success: false });
+
+        const user = req.user;
+
+        const expenses = await user.getExpenses({ where: { id: expenseId } });
         const expense = expenses[0];
-        const result = await expense.destroy();
+        await expense.destroy({ transaction: t });
+
+        user.totalExpense = Number(user.totalExpense) - Number(expense.amount);
+
+        await user.save({ transaction: t });
+
+        await t.commit();
         return res.status(200).json({ success: true });
-    } catch(err) {
-        // console.log(err);
+    } catch (err) {
+        console.log(err);
+        await t.rollback();
         return res.status(500).json({ message: "Some error occurred", success: false });
     }
 }
