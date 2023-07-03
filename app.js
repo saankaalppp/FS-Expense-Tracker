e0e245e4aa5af47ef733809ab6cff764bde647af
saankaalppp <sankalp18v@gmail.com>
2023-07-03 19:47:18 +0530

    Deploying It

--- app.js
+++ app.js
@@ -8,24 +8,32 @@ const purchaseRoutes = require('./routes/purchase');
 const sequelize = require('./util/database');
 const express = require('express');
 const path = require('path');
+const fs= require ('fs');
 const cors = require('cors');
 const Entry = require('./models/entry');
 const User = require('./models/user');
 const FilesDownloaded = require('./models/filesdownloaded');
 const ForgotPasswordRequests = require('./models/forgotpasswordrequest');
-const jade = require('ejs');
+const ejs = require('ejs');
+const helmet= require('helmet');
+const morgan= require ('morgan');
 const Order = require('./models/order');
 const Razorpay = require('razorpay');
 require('dotenv').config();
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 app.set('view engine', 'ejs');
 app.set('views', path.join(__basedir, 'views', '/auth'));
 
 app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
+app.use(helmet());
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use('/user', userRoutes);
 app.use('/', dashboardRoutes);
 app.use('/premium', premiumRoutes);
